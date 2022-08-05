12a9ce238f6c290ad7f01719d673df5e48ed8dc6
alpi2022 <110289798+alpi2022@users.noreply.github.com>
2022-08-05 17:30:01 +0100

    Add files via upload

---
+++ NewEmptyFile.java
@@ -0,0 +1,3 @@
+public class NewEmptyFile {
+    //comment
+}
---
+++ NewFunction.java
@@ -0,0 +1,8 @@
+public class NewFunction {
+
+    public static void main(String[] args) {
+
+        System.out.println("hello coders how are you");
+
+    }
+}
